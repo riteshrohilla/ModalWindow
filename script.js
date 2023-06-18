be184c3b5d13f5ec3521c964f2e5751d5aab977a
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-06-18 14:30:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
 for (let i = 0; i < btnsShowModal.length; i++)
-    console.log(btnsShowModal[i].textContent);
+    btnsShowModal[i].addEventListener('click', function () {
+        console.log('Button clicked!');
+    });
