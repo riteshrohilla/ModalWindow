0ffb6f2ef3740237eee423dcef0eb504833ac9a7
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-06-18 15:11:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,11 @@ for (let i = 0; i < btnsShowModal.length; i++)
         overlay.classList.remove("hidden");
     });
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-});
+}
 
-overlay.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
