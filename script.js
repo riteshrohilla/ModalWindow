c68ec3416e81c24e743a7a2c3272cb098b292a07
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-10 02:02:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,5 +35,5 @@ document.addEventListener('keydown', function (e) {
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsShowModal = document.querySelector('.show-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
