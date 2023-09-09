6f14d630281c41b71de7dc40aea67f2dcf37bea6
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-10 01:54:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,5 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
 const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsShowModal = document.querySelector('.open-modal');
