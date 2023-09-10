d157c5e99ef091e7b69ecc97c3b97ed41b410878
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-10 11:58:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,8 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
-for (let i = 0; i < btnsShowModal.length; i++) {
-    console.log(btnsShowModal[i].textContent);
-}
+for (let i = 0; i < btnsShowModal.length; i++)
+    // console.log(btnsShowModal[i].textContent);
+    btnsShowModal[i].addEventListener('click', function () {
+        console.log('Button Clicked');
+    });
